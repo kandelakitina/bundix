0f25b101fe51dd6bf789c173a6df3f4b29c710be
Xilef <zen9.felix@gmail.com>
2024-04-10 12:50:48 +0800

    Remove irrelevant rails files

--- test/apps/rails-app/lib/phony_gem/lib/phony_gem.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'phony_gem/version'
-
-module PhonyGem
-  class Error < StandardError; end
-
-  # Your code goes here...
-end
--- test/apps/rails-app/lib/phony_gem/lib/phony_gem/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module PhonyGem
-  VERSION = '0.1.0'
-end
