00aa44358cd1eb37858be973f8ba26008da3dfe5
boticelli <kandelakitina@gmail.com>
2025-09-16 15:48:42 +0300

    Remove minitest from bundix

--- flake.nix
+++ flake.nix
@@ -5,41 +5,46 @@
     "Generates a Nix expression for your Bundler-managed application";
 
   outputs = { self, nixpkgs, ... }:
-  let
-    eachDefaultSystem = with nixpkgs.lib; let
-      nestInto = name: val: { ${name} = val; };
-      specialize = block: sys: mapAttrs (_: nestInto sys) (block sys);
-      mergeSets = foldl recursiveUpdate {};
-      forEachSystem = forEach systems.flakeExposed;
-    in
-      perSystemBlock: mergeSets (forEachSystem (specialize perSystemBlock));
+    let
+      eachDefaultSystem = with nixpkgs.lib;
+        let
+          nestInto = name: val: { ${name} = val; };
+          specialize = block: sys: mapAttrs (_: nestInto sys) (block sys);
+          mergeSets = foldl recursiveUpdate { };
+          forEachSystem = forEach systems.flakeExposed;
+        in perSystemBlock:
+        mergeSets (forEachSystem (specialize perSystemBlock));
 
-  in
-    eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+    in eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-      rubyCurrent = pkgs.ruby_3_1;
+        rubyCurrent = pkgs.ruby_3_1;
 
-      ruby = rubyCurrent.withPackages (ps: with ps; [
-        minitest rake solargraph rubocop pry
-      ]);
+        ruby = rubyCurrent.withPackages (ps:
+          with ps; [
+            # minitest
+            rake
+            solargraph
+            rubocop
+            pry
+          ]);
 
-      bundler = pkgs.bundler.override { ruby = rubyCurrent; };
+        bundler = pkgs.bundler.override { ruby = rubyCurrent; };
 
-      bundix = pkgs.callPackage ./default.nix {
-        inherit pkgs ruby bundler;
-        inherit (pkgs) nix nix-prefetch-git;
-      };
-    in {
-      packages.default = bundix;
+        bundix = pkgs.callPackage ./default.nix {
+          inherit pkgs ruby bundler;
+          inherit (pkgs) nix nix-prefetch-git;
+        };
+      in {
+        packages.default = bundix;
 
-      apps.default = {
-        type = "app";
-        program = nixpkgs.lib.getExe bundix;
-      };
+        apps.default = {
+          type = "app";
+          program = nixpkgs.lib.getExe bundix;
+        };
 
-      devShells.default = pkgs.mkShell {
-        buildInputs = [ ruby bundler bundix pkgs.rufo ];
-      };
-    });
+        devShells.default =
+          pkgs.mkShell { buildInputs = [ ruby bundler bundix pkgs.rufo ]; };
+      });
 }
